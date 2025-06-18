e006f002b6047dc75fc71f4617e2b963673d04dd
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2025-06-18 20:22:35 +0530

    Add files via upload

---
+++ module-1/demo-java/src/day/one/Operators.java
@@ -0,0 +1,33 @@
+package day.one;
+
+public class Operators {
+    public static void main(String[] args) {
+        // operators -> + - * / %(module) == != > < >= <= >> << ++ --
+
+//        int x = 8;
+
+//        System.out.println(x >> 1);
+
+
+        int x = 10;
+//        x = x + 1;
+//        x++;
+//        System.out.println(++x);
+//        System.out.println(x++);
+//        System.out.println(x);
+
+
+//        x--;
+//        System.out.println(x);
+
+//        x = x + 2;
+//        x += 2;
+
+        x -= 2;
+        x *= 2;
+
+        System.out.println(x);
+
+
+    }
+}
---
+++ module-1/demo-java/src/day/two/Loops.java
@@ -0,0 +1,56 @@
+package day.two;
+
+public class Loops {
+    public static void main(String[] args) {
+        // loops -> while , for , do while
+
+//        while (true){
+//            System.out.println("from if");
+//        }
+
+//        int counter = 3;
+//        while (counter > 0) {
+//            System.out.println(counter);
+//            counter--;
+//        }
+
+//        for (int counter = 3;counter > 0; counter--){
+//            System.out.println(counter);
+//        }
+
+//        int counter = 0;
+//        do {
+//            System.out.println(counter);
+//            counter--;
+//        }while (counter > 0);
+
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5; j++) {
+//                System.out.println(j); // 1[5] 2[5] 3[5] 4[5] 5[5]
+//            }
+//        }
+
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5; j++) {
+//                for (int k = 1; k <= 5; k++) {
+//                    System.out.println(k);
+//                }
+//            }
+//        }
+
+
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= i ; j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i ; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
