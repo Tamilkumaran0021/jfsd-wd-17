56f27d8de62cd1653be14c60def1b36bbf0e5173
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2025-06-19 20:27:44 +0530

    day - 3

---
+++ module-1/demo-java/src/day/three/Functions1.java
@@ -0,0 +1,11 @@
+package day.three;
+
+public class Functions1 {
+
+    public static void main(String[] args) {
+        // objects or instance variables
+
+        Functions func = new Functions();
+        func.add(100, 200);
+    }
+}
--- module-1/demo-java/src/day/two/Loops.java
+++ module-1/demo-java/src/day/two/Loops.java
@@ -38,19 +38,35 @@ public class Loops {
 //            }
 //        }
 
+//        int k = 1;
 
 //        for (int i = 1; i <= 5; i++) {
-//            for (int j = 1; j <= i ; j++) {
+//            for (int j = 1; j <= i; j++) {
+//                System.out.print(k);
+//                k++;
+//            }
+//            System.out.println();
+//        }
+
+//        for (int i = 1; i <= 5; i++) {
+//            for (int j = 1; j <= 5 - i ; j++) {
 //                System.out.print(j);
 //            }
 //            System.out.println();
 //        }
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5 - i ; j++) {
-                System.out.print(j);
-            }
-            System.out.println();
-        }
+
+//        for (int i = 1; i <= 4; i++) {
+//            for (int j = 1; j <= 4 - i; j++) {
+//                System.out.print(" ");
+//            }
+//
+//            for (int k = 1; k <= i; k++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+
     }
 }
