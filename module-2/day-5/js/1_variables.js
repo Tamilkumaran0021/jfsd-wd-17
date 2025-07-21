11aa524a84156638693d227e6a242c7f2d420a75
Sarath Kumar Ezilarasan <sarathezil@gmail.com>
2025-07-21 20:34:02 +0530

    day - 22

---
+++ module-2/day-5/js/1_variables.js
@@ -0,0 +1,45 @@
+// automatically , var , let , const
+
+// x = 5;
+// y = 6;
+
+// console.log(x + y);
+
+// var x = 5;
+// var y = 6;
+
+// console.log(x + y);
+
+// let x = 5;
+// let y = 6;
+
+// console.log(x + y);
+
+// const x = 5;
+// x = 6;
+
+// console.log(x);
+
+///////////////////////////////////////////////////////
+// let x;
+// x = 6;
+
+// console.log(x);
+
+// var x = 6;
+// var x;
+
+// console.log(x);
+
+// var x = 5;
+
+// {
+//   var x = 6;
+//   console.log(x);
+// }
+// console.log(x);
+
+x = 6;
+var x;
+
+console.log(x);
